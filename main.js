88053d3c5e5e25b0c701e001e442114c4c110f34
Gonfidel <43705712+Gonfidel@users.noreply.github.com>
2019-10-13 00:05:51 -0600

    added star and pages badge

--- main.js
+++ main.js
@@ -19,11 +19,14 @@ async function getBookData(){
         data.items.forEach(function(book){
           console.log(`Book: ${book.volumeInfo.title}`);
           renderDom.innerHTML+=`
-          <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+          <div class="col-sm-12 col-md-6 col-lg-3 p-3 mb-5 mx-3 shadow-sm bg-light">
             <img class="d-block mx-auto" style="height: 10em;" src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="thumbnail" />
             <hr />
             <h6 class="text-center">${book.volumeInfo.title}</h6>
             <p class="text-center">Author: ${book.volumeInfo.authors[0]}</p>
+            <span class="float-left"><i class="material-icons" style="color: rgb(248, 211, 0);">star</i> ${book.volumeInfo.averageRating!=undefined?book.volumeInfo.averageRating+"/5":"N/A"}</span>
+            <span class="badge badge-danger badge-pill float-right">Pages: ${book.volumeInfo.pageCount!=undefined?book.volumeInfo.pageCount:"Unknown"}</span>
+
           </div>
 
           `;
