73853bab5437cfd152193ce613e82f6b4e58a81d
Gonfidel <43705712+Gonfidel@users.noreply.github.com>
2019-10-13 00:07:36 -0600

    adjust card shadow on book results

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ async function getBookData(){
         data.items.forEach(function(book){
           console.log(`Book: ${book.volumeInfo.title}`);
           renderDom.innerHTML+=`
-          <div class="col-sm-12 col-md-6 col-lg-3 p-3 mb-5 mx-3 shadow-sm bg-light">
+          <div class="col-sm-12 col-md-6 col-lg-3 p-3 mb-5 mx-3 shadow bg-light">
             <img class="d-block mx-auto" style="height: 10em;" src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="thumbnail" />
             <hr />
             <h6 class="text-center">${book.volumeInfo.title}</h6>
